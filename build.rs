5f85a9f5b55ba30cfda49cc16715bd3d7693dace
humbinal <humbinal@126.com>
2024-06-26 23:34:23 +0800

    publish v0.1.1

--- build.rs
+++ build.rs
@@ -3,7 +3,10 @@ use winresource;
 use winresource::WindowsResource;
 
 fn main() -> io::Result<()> {
-    slint_build::compile("ui/app.slint").unwrap();
+    // slint_build::compile("ui/app.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new()
+        .with_style("fluent-light".into());
+    slint_build::compile_with_config("ui/app.slint", config).unwrap();
     #[cfg(windows)]{
         WindowsResource::new()
             .set_icon("resources/logo.ico")
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,18 @@ use chrono::{DateTime, Local};
 use rsntp::SntpClient;
 use slint::{SharedString, Timer, TimerMode};
 
+use winit_helper::center_window;
+
+mod winit_helper;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+
+    ui.show()?;
+
+    center_window(ui.window());
 
     let ui_handle = ui.as_weak();
 
@@ -19,29 +27,29 @@ fn main() -> Result<(), slint::PlatformError> {
             println!("check button clicked...");
             let ui = ui_handle.unwrap();
             ui.set_check_button_enabled(false);
-            ui.set_check_result(SharedString::from(""));
             let ntp_server = ui.get_ntp_server();
-            println!("{}", format!("ntp_server1: {}", ntp_server));
-
+            ui.set_check_result(SharedString::from(format!("服务器{}连接中...", ntp_server)));
             let ui_handle = ui.as_weak();
             thread::spawn(move || {
-                println!("{}", format!("ntp_server2: {}", ntp_server));
                 let mut client = SntpClient::new();
                 client.set_timeout(Duration::from_secs(3));
                 let result = client.synchronize(ntp_server.to_string());
-                let mut result_str = "失败".to_string();
-                match result {
+                let (result_msg, succeed) = match result {
                     Ok(result) => {
                         let local_time: DateTime<Local> =
                             DateTime::from(result.datetime().into_chrono_datetime().unwrap());
-                        result_str = local_time.format("%Y-%m-%d %H:%M:%S").to_string();
+                        let res_time = local_time.format("%Y-%m-%d %H:%M:%S").to_string();
+                        (format!("服务器{}连接成功!\n返回时间为: {}", ntp_server, res_time), true)
                     }
-                    Err(_) => {}
-                }
+                    Err(error) => {
+                        (format!("服务器{}连接失败!\n{}", ntp_server, error), false)
+                    }
+                };
 
                 let _ = slint::invoke_from_event_loop(move || {
                     let ui = ui_handle.clone().unwrap();
-                    ui.set_check_result(SharedString::from(format!("测试结果: {}", result_str)));
+                    ui.set_check_succeed(succeed);
+                    ui.set_check_result(SharedString::from(result_msg));
                     ui.set_check_button_enabled(true);
                 });
             });
@@ -49,24 +57,9 @@ fn main() -> Result<(), slint::PlatformError> {
     });
 
     let ui_handle = ui.as_weak();
-    /* thread::spawn(move || {
-         loop {
-             let time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-             if let Some(ui) = ui_handle.upgrade() {
-                 ui.set_time(SharedString::from(time.clone()));
-                 // 等待1秒
-                 thread::sleep(Duration::from_secs(1));
-                 println!("22222222222222222");
-             } else {
-                 // 如果 UI 已经关闭，退出循环
-                 println!("3333333333333333333333333333");
-                 break;
-             }
-         }
-     });*/
     let timer = Timer::default();
-    timer.start(TimerMode::Repeated, std::time::Duration::from_millis(100), move || {
-        // println!("This will be printed every 500ms.");
+    timer.start(TimerMode::Repeated, std::time::Duration::from_millis(200), move || {
+        // println!("This will be printed every 200ms.");
         let ui = ui_handle.unwrap();
         let time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
         ui.set_time(SharedString::from(time.clone()));
